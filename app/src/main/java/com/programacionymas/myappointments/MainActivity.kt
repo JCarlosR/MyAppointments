f9ef7371b319dff2a01e5c40cc6ce60b37778f13
Juan Ramos <juancagb.17@gmail.com>
2018-11-20 21:42:34 -0500

    Add snackBar and override fun onBackPressed

--- app/src/main/java/com/programacionymas/myappointments/MainActivity.kt
+++ app/src/main/java/com/programacionymas/myappointments/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.programacionymas.myappointments
 
-import android.content.Context
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
 import com.programacionymas.myappointments.PreferenceHelper.get
 import com.programacionymas.myappointments.PreferenceHelper.set
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val snackBar by lazy {
+        Snackbar.make(mainLayout, R.string.press_back_again, Snackbar.LENGTH_SHORT)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,10 +23,6 @@ class MainActivity : AppCompatActivity() {
         // SQLite
         // files
 
-        /*
-        val preferences = getSharedPreferences("general", Context.MODE_PRIVATE)
-        val session = preferences.getBoolean("session", false)
-        */
         val preferences = PreferenceHelper.defaultPrefs(this)
         if (preferences["session", false])
             goToMenuActivity()
@@ -42,12 +42,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun createSessionPreference() {
-        /*
-        val preferences = getSharedPreferences("general", Context.MODE_PRIVATE)
-        val editor = preferences.edit()
-        editor.putBoolean("session", true)
-        editor.apply()
-        */
         val preferences = PreferenceHelper.defaultPrefs(this)
         preferences["session"] = true
     }
@@ -56,5 +50,12 @@ class MainActivity : AppCompatActivity() {
         val intent = Intent(this, MenuActivity::class.java)
         startActivity(intent)
         finish()
+    }
+
+    override fun onBackPressed() {
+        if (snackBar.isShown)
+            super.onBackPressed()
+        else
+            snackBar.show()
     }
 }
