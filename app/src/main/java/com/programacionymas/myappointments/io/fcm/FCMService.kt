0aa934e1bef67510076e14ee7f63187d37b26dae
Juan Ramos <juancagb.17@gmail.com>
2018-11-28 22:02:27 -0500

    Firebase Cloud Messaging

---
+++ app/src/main/java/com/programacionymas/myappointments/io/fcm/FCMService.kt
@@ -0,0 +1,100 @@
+package com.programacionymas.myappointments.io.fcm
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.os.Build
+import android.support.v4.app.NotificationCompat
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.programacionymas.myappointments.R
+import com.programacionymas.myappointments.ui.MainActivity
+
+
+class FCMService : FirebaseMessagingService() {
+
+    /**
+     * Called when message is received.
+     *
+     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
+     */
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        // There are two types of messages data messages and notification messages. Data messages are handled
+        // here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
+        // traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
+        // is in the foreground. When the app is in the background an automatically generated notification is displayed.
+        // When the user taps on the notification they are returned to the app. Messages containing both notification
+        // and data payloads are treated as notification messages.
+
+        // Handle FCM messages here.
+        Log.d(TAG, "From: " + remoteMessage.from)
+
+        // Check if message contains a data payload.
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.data)
+
+            handleNow()
+        }
+
+        // Check if message contains a notification payload.
+        remoteMessage.notification.let {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.notification?.body)
+        }
+
+        // sendNotification()
+    }
+    // [END receive_message]
+
+    /**
+     * Handle time allotted to BroadcastReceivers.
+     */
+    private fun handleNow() {
+        Log.d(TAG, "Short lived task is done.")
+    }
+
+    /**
+     * Create and show a simple notification containing the received FCM message.
+     *
+     * @param messageBody FCM message body received.
+     */
+    private fun sendNotification(messageBody: String) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0 /* Request code */, intent,
+            PendingIntent.FLAG_ONE_SHOT
+        )
+
+        val channelId = getString(R.string.default_notification_channel_id)
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_schedule)
+            .setContentTitle("FCM Message")
+            .setContentText(messageBody)
+            .setAutoCancel(true)
+            .setSound(defaultSoundUri)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager?
+
+        // Since android Oreo notification channel is needed.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                "Channel human readable title",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager!!.createNotificationChannel(channel)
+        }
+
+        notificationManager!!.notify(0 /* ID of notification */, notificationBuilder.build())
+    }
+
+    companion object {
+        private const val TAG = "FCMService"
+    }
+}
--- app/src/main/java/com/programacionymas/myappointments/ui/MainActivity.kt
+++ app/src/main/java/com/programacionymas/myappointments/ui/MainActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Toast
+import com.google.firebase.iid.FirebaseInstanceId
 import com.programacionymas.myappointments.util.PreferenceHelper
 import com.programacionymas.myappointments.util.PreferenceHelper.get
 import com.programacionymas.myappointments.util.PreferenceHelper.set
@@ -30,6 +32,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseInstanceId.getInstance().instanceId.addOnSuccessListener(this) { instanceIdResult ->
+            val deviceToken = instanceIdResult.token
+            Log.d("FCMService", deviceToken)
+        }
 
         // shared preferences
         // SQLite
